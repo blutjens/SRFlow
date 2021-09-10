e5d2988092f81ccbbf95a2d369b0cfbacbadccc0
blutjens <bjoern.luetjens@gmail.com>
2021-09-10 16:58:20 -0400

    switched venv to conda

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -2,6 +2,7 @@
 
 set -e # exit script if an error occurs
 
-source myenv/bin/activate # Install with ./setup.sh
+#source myenv/bin/activate # Install with ./setup.sh
+conda activate superbench
 cd code
 python -m jupyter notebook demo_on_pretrained.ipynb # Start jupyter using the python from the virtual environment
